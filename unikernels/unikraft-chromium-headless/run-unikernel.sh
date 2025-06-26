8bc51955d8d963baa6a755c1700fab69842adbfc
Rafael Garcia <raf@onkernel.com>
2025-06-26 05:52:08 -0400

    create inst

--- unikernels/unikraft-chromium-headless/run-unikernel.sh
+++ unikernels/unikraft-chromium-headless/run-unikernel.sh
@@ -8,11 +8,11 @@ name="chromium-headless-test"
 CHROMIUM_FLAGS="--disable-field-trial-config --disable-background-networking --disable-background-timer-throttling --disable-backgrounding-occluded-windows --disable-back-forward-cache --disable-breakpad --disable-client-side-phishing-detection --disable-component-extensions-with-background-pages --disable-component-update --no-default-browser-check --disable-default-apps --disable-dev-shm-usage --disable-extensions --disable-features=AcceptCHFrame,AutoExpandDetailsElement,AvoidUnnecessaryBeforeUnloadCheckSync,CertificateTransparencyComponentUpdater,DeferRendererTasksAfterInput,DestroyProfileOnBrowserClose,DialMediaRouteProvider,ExtensionManifestV2Disabled,GlobalMediaControls,HttpsUpgrades,ImprovedCookieControls,LazyFrameLoading,LensOverlay,MediaRouter,PaintHolding,ThirdPartyStoragePartitioning,Translate --allow-pre-commit-input --disable-hang-monitor --disable-ipc-flooding-protection --disable-popup-blocking --disable-prompt-on-repost --disable-renderer-backgrounding --force-color-profile=srgb --metrics-recording-only --no-first-run --enable-automation --password-store=basic --use-mock-keychain --no-service-autorun --export-tagged-pdf --disable-search-engine-choice-screen --unsafely-disable-devtools-self-xss-warnings --enable-use-zoom-for-dsf=false --use-angle --hide-scrollbars --mute-audio --blink-settings=primaryHoverType=2,availableHoverTypes=2,primaryPointerType=4,availablePointerTypes=4 --no-sandbox --disable-blink-features=AutomationControlled --accept-lang=en-US,en --no-startup-window"
 
 kraft cloud inst create \
-  --start 
-	-M 8192 \
-	-p 443:9222/http+tls \
-	-e CHROMIUM_FLAGS="$CHROMIUM_FLAGS" \
-     --vcpus 1 \
-     -M 1.5Gi \
-	-n "$name" \
-    $image
+  --start \
+  -M 8192 \
+  -p 443:9222/http+tls \
+  -e CHROMIUM_FLAGS="$CHROMIUM_FLAGS" \
+  --vcpus 1 \
+  -M 1.5Gi \
+  -n "$name" \
+  $image
