601ce91e9fb7c8d358cc0791ab509b03e83407cb
Rafael Garcia <rgarcia2009@gmail.com>
2025-07-14 14:49:41 -0700

    get rid of no-sandbox warning, run on 8080 in the foreground

--- images/chromium-headful/run-docker.sh
+++ images/chromium-headful/run-docker.sh
@@ -23,7 +23,7 @@ RUN_ARGS=(
   -e DISPLAY_NUM=1 \
   -e HEIGHT=768 \
   -e WIDTH=1024 \
-  -e CHROMIUM_FLAGS="--no-sandbox --disable-dev-shm-usage --disable-gpu --start-maximized --disable-software-rasterizer --remote-allow-origins=* --no-zygote"
+  -e CHROMIUM_FLAGS="--user-data-dir=/home/kernel/user-data --disable-dev-shm-usage --disable-gpu --start-maximized --disable-software-rasterizer --remote-allow-origins=*"
 )
 
 if [[ "${WITH_KERNEL_IMAGES_API:-}" == "true" ]]; then
@@ -34,7 +34,7 @@ fi
 # noVNC vs WebRTC port mapping
 if [[ "${ENABLE_WEBRTC:-}" == "true" ]]; then
   echo "Running container with WebRTC"
-  RUN_ARGS+=( -p 443:8080 )
+  RUN_ARGS+=( -p 8080:8080 )
   RUN_ARGS+=( -e ENABLE_WEBRTC=true )
   if [[ -n "${NEKO_ICESERVERS:-}" ]]; then
     RUN_ARGS+=( -e NEKO_ICESERVERS="$NEKO_ICESERVERS" )
@@ -49,4 +49,4 @@ else
 fi
 
 docker rm -f "$NAME" 2>/dev/null || true
-docker run -d "${RUN_ARGS[@]}" "$IMAGE"
+docker run -it "${RUN_ARGS[@]}" "$IMAGE"
