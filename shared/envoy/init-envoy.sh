01d7303e72e9fef05f7054a0aaae47a0e8ffd64b
Steven Miller <sjmiller609@gmail.com>
2025-09-25 11:06:53 -0400

    Update shared/envoy/init-envoy.sh

--- shared/envoy/init-envoy.sh
+++ shared/envoy/init-envoy.sh
@@ -95,14 +95,3 @@ sed -e "s|{INST_NAME}|$inst_esc|g" \
 
 echo "[envoy-init] Starting Envoy via supervisord"
 supervisorctl -c /etc/supervisor/supervisord.conf start envoy
-echo "[envoy-init] Waiting for Envoy admin on 127.0.0.1:9901..."
-for i in {1..50}; do
-  if (echo >/dev/tcp/127.0.0.1/9901) >/dev/null 2>&1; then
-    echo "[envoy-init] Envoy is started"
-    break
-  fi
-  sleep 0.1
-  if [[ $i -eq 50 ]]; then
-    echo "[envoy-init] Failed to start Envoy - admin interface not responding after 5 seconds"
-  fi
-done
