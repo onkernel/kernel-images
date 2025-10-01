79f83a0562b529d6d2c90db9ece8993c6a3345fd
Hiro Tamada <hiro@onkernel.com>
2025-10-01 13:38:05 -0700

    Change to 1366x768

--- images/chromium-headful/run-docker.sh
+++ images/chromium-headful/run-docker.sh
@@ -37,8 +37,8 @@ RUN_ARGS=(
   -p 9222:9222
   -p 444:10001
   -e DISPLAY_NUM=1
-  -e HEIGHT=2160
-  -e WIDTH=3840
+  -e HEIGHT=768
+  -e WIDTH=1366
   -e RUN_AS_ROOT="$RUN_AS_ROOT"
   --mount type=bind,src="$FLAGS_FILE",dst=/chromium/flags,ro
 )
